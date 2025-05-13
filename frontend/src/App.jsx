d06cf725365c23addb6f89c8a9eb287e719f8086
Saurabh-112233 <comraded91@gmail.com>
2025-05-13 21:56:17 +0530

    just a change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import "./App.css";
 import Forms from "./components/Forms";
 import RoomPage from "./pages/RoomPage";
 
-const server = "http://localhost:5000";
+const server = "https://whiteboard-ohuo.onrender.com/";
 const connectionOptions = {
   "force new connection": true,
   reconnectionAttempts: "Infinity",
